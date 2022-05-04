cf6e92b9fa8b3041003a1141229e2fba9d8311f2
valentin-ruggieri <valeru.251@gmail.com>
2022-05-04 03:27:07 -0300

    Desafio 04 (parte 1)

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const router = require('./productosRouter');
+const PORT = 8000;
+
+app.listen(PORT,()=>{
+    console.log(`Servidor listo en el puerto ${PORT} ✅`);
+})
+
+
